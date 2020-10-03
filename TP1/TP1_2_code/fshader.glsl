4b9ad458ce0053570fd6daf81eb7c76e8d8f292d
Luc SENECAL <luc.senecal14@wanadoo.fr>
2020-10-03 13:05:35 +0200

    FEATURE: heighmap dans les shaders

--- TP1/TP1_2_code/fshader.glsl
+++ TP1/TP1_2_code/fshader.glsl
@@ -1,14 +1,23 @@
 #version 150
 
-uniform sampler2D texture;
+uniform sampler2D texture_grass;
+uniform sampler2D texture_rock;
+uniform sampler2D texture_snow;
 
 in vec2 v_texcoord;
-in vec3 v_position;
+in vec4 v_position;
+
 //! [0]
 void main()
 {
     // Set fragment color from texture
-    gl_FragColor = texture2D(texture, v_texcoord);
+    if (v_position.y > 0.8f)
+        gl_FragColor = texture2D(texture_snow, v_texcoord);
+    else if (v_position.y > 0.2f)
+        gl_FragColor = texture2D(texture_rock, v_texcoord);
+    else
+        gl_FragColor = texture2D(texture_grass, v_texcoord);
+
 }
 //! [0]
 
